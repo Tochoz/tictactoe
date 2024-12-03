d779bfbcd172732e3d36926ed65b72660f7fd361
mantevian <zmantevian@gmail.com>
2024-12-03 14:20:52 +0300

    pages

---
+++ auth.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+	<title>Крестики-нолики</title>
+</head>
+
+<body>
+	<h1>Авторизация</h1>
+
+	<form>
+		<input type="text" name="login">
+		<input type="password" name="password">
+		<input type="submit" value="Авторизоваться">
+	</form>
+</body>
+
+</html>
